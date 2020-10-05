a521d446057ca78cd6f21f3ed0345a9787752a26
apple <1968895380@qq.com>
2020-10-05 22:42:46 +0800

    更改nacos的文件，直接刷新

--- cloud-config-nacos-client-3377/src/main/java/com/lihewei/ConfigNacosClient3377.java
+++ cloud-config-nacos-client-3377/src/main/java/com/lihewei/ConfigNacosClient3377.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * 更改nacos的文件，直接刷新
+ */
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableDiscoveryClient
 public class ConfigNacosClient3377 {
